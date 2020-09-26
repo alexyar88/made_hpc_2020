88285411ebf6c3c33a8bb6f48f8e4e8489ffad91
alexyar88 <altmer88@mail.ru>
2020-09-26 15:06:26 +0300

    matrix matmul

--- hw2/matrix_dot_product/build.sh
+++ hw2/matrix_dot_product/build.sh
@@ -6,4 +6,8 @@ g++ -c vector_row.cpp
 g++ -c dot_product.cpp
 g++ -c dot_product_strassen.cpp
 ar rc matrix_lib.a matrix.o vector_col.o vector_row.o dot_product.o dot_product_strassen.o
-g++ -std=c++17 main.cpp matrix_lib.a -o matmul.bin
+g++ -std=c++17 main.cpp matrix_lib.a -o matmul
+
+# Скопируем в папку для того, чтобы измерять скорость.
+mkdir -p ../speed_measurement
+cp matmul ../speed_measurement/matmul
